522920c79a96f8e5c07818497656cb2431d86514
CodingKaiser <falkonoe@live.com>
2017-04-29 18:38:10 -0700

    Added code. Haven't tested.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Avocado BBW", false, "2017-02-28 17:28:43");
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Salmon", false, "2017-03-18 14:28:43");
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Chicken", false, "2016-02-15 11:23:13");
